219443046fc92c25e1aeb85aafeed19f7135dedd
Nelson Malbone <nhmalbone@icloud.com>
2024-09-12 19:07:39 -0400

    BroCode Countdown Timer Date: 09/12/2024
    with  main 2 is just screwing around with some ideas

---
+++ main2.py
@@ -0,0 +1,13 @@
+# self built timer
+
+# users input for hr, min, secs
+# learn that your cant have user enter inputs like this so new method
+import time
+
+
+"""
+all_times = input('Enter times in HH:MM:SS\n').split()
+for time in all_times:
+    hour, minute, sec = [int(i) for i in time.split(":")]
+    print(hour, "hours", minute, "minutes", sec, "seconds")
+"""
